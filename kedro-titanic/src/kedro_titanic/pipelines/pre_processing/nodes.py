3bdf82b914c770d3d00bef673a55400bc44d6eef
tadeu <tadeucbmad@gmail.com>
2022-07-01 02:06:16 -0400

    new commit

--- kedro-titanic/src/kedro_titanic/pipelines/pre_processing/nodes.py
+++ kedro-titanic/src/kedro_titanic/pipelines/pre_processing/nodes.py
@@ -14,3 +14,20 @@ def feature_engineering(df: pd.DataFrame):
     df['Title'] = df['Name'].apply(lambda x: str(x).split(',')[1].split('.')[0])
 
     return df
+
+def filter_dataset(df: pd.DataFrame):
+    """This Function Filter Columns"""
+    df = df[['Pclass', 'Sex', 'Age', 'SibSp', 
+             'Parch', 'Fare', 'Embarked', 
+             'Region_Cabin', 'Title', 'Survived']]
+
+    return df
+
+def encoding_dataset(df: pd.DataFrame):
+    """This Function Encode Columns"""
+    df = pd.get_dummies(df, 's_', columns=['Sex'], drop_first=True)
+    df = pd.get_dummies(df, 't_', columns=['Title'], drop_first=True)
+    df = pd.get_dummies(df, 'r_', columns=['Region_Cabin'], drop_first=True)
+    df = pd.get_dummies(df, 'e_', columns=['Embarked'], drop_first=True)
+
+    return df
--- kedro-titanic/src/kedro_titanic/pipelines/pre_processing/pipeline.py
+++ kedro-titanic/src/kedro_titanic/pipelines/pre_processing/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.1
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import feature_engineering
+from .nodes import feature_engineering, filter_dataset, encoding_dataset
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,5 +14,17 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs = 'train',
             outputs = 'fe_train',
             name = 'df_featured'
+        ),
+        node(
+            func = filter_dataset,
+            inputs = 'fe_train',
+            outputs = 'filtered_train',
+            name = 'df_filtered'
+        ),
+        node(
+            func = encoding_dataset,
+            inputs = 'filtered_train',
+            outputs = 'encoded_train',
+            name = 'df_encoded'
         )
     ])
